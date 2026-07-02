a21ec9fde477e1099b3406f4ae049bcd1fc5089f
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-03 01:32:07 +0530

    added section for about and project pages with placeholder content

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
-import React from 'react'
-
-const page = () => {
+export default function Page() {
   return (
     <main className='h-[200vh] w-full '>
       <h1 className='text-4xl'>Welcome to the Page</h1>
     </main>
-  )
+  );
 }
-
-export default page
